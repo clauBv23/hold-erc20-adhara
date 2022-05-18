5bebbd477e8da86012f295acc3c947f673b79b09
clau <claudiabarcelovaldes40@gmail.com>
2022-05-18 19:24:20 +0200

    Feat: Add openZeppelin contracts dependency

--- HolderERC20/contracts/HolderERC20.sol
+++ HolderERC20/contracts/HolderERC20.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract HolderERC20 is ERC20Upgradeable {
+    using Counters for Counters.Counter;
+
     constructor() {}
 }
