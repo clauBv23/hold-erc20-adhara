05d846087cb2c84fec7c0ae62eba223aa8da5613
clau <claudiabarcelovaldes40@gmail.com>
2022-05-18 19:22:29 +0200

    Feat: Add the Holder ERC20 contract and the structure definition

---
+++ HolderERC20/contracts/HolderERC20.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+
+import "hardhat/console.sol";
+
+contract HolderERC20 is ERC20Upgradeable {
+    constructor() {}
+}
