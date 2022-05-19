4fad279a756356f57b0acc627b3b0d0e251dadb0
clau <claudiabarcelovaldes40@gmail.com>
2022-05-19 02:54:02 +0200

    feature: react init configuration and metamask connection configuration

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Connector from './Connector'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Connector></Connector>
+
+    </div>
+  );
+}
+
+export default App;
